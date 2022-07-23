f15532821dd06ce0b2d55ff84088d54a9e9d0b8a
souenxx <souen0823@gmail.com>
2022-07-24 01:04:42 +0900

    chore: add ApolloProvider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { Top } from "../src/components/pages/Top";
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://api.github.com/graphql",
+  headers: {
+    authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+  },
+  resolvers: {},
+});
 
 export default function Home() {
-  return <Top />;
+  return (
+    <ApolloProvider client={client}>
+      <Top />
+    </ApolloProvider>
+  );
 }
